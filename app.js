d2f1a9fef511424e68e5165dde4287f5fc74990d
Joseph <jhl843@nyu.edu>
2020-04-01 09:51:46 -0400

    port update

---
+++ app.js
@@ -0,0 +1,12 @@
+// Import Express
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
+
+// Create base route
+app.get('/', (req, res) => res.send('Exercise Four'));
+
+// Set up app so that it runs when this file is run
+app.listen(port, () => 
+    console.log(`Example app listening at http://localhost:${port}`)
+);
