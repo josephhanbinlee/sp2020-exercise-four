5b0b325e386b42051eea6506c8e5c795fe7b2101
Joseph <jhl843@nyu.edu>
2020-04-08 09:26:59 -0400

    pushing

--- app.js
+++ app.js
@@ -4,8 +4,42 @@ const app = express()
 
 const port = process.env.PORT || 4000;
 
+// Require Firebase
+const firebase = require("firebase");
+
+// Get config so we can communicate with Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDRBxZvPJaI1Zzy9UeSbqUDYQO8j0xf_j0",
+  authDomain: "exercise-four-b02d1.firebaseapp.com",
+  databaseURL: "https://exercise-four-b02d1.firebaseio.com",
+  projectId: "exercise-four-b02d1",
+  storageBucket: "exercise-four-b02d1.appspot.com",
+  messagingSenderId: "570213342156",
+  appId: "1:570213342156:web:b1038f696e7d9e28e75b69",
+  measurementId: "G-PR9KRRXDYG"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Firestore Database
+const db = firebase.firestore()
+
+// Get Blog Posts
+const blogposts = db
+  .collection('blogposts')
+  .get()
+  .then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+    });
+  })
+  .catch( function(error) {
+    console.log('Error:', error)
+  });
+
 // Create base route
-app.get('/', (req, res) => res.send('Exercise Four'));
+app.get('/', (req, res) => res.send('Data for Exercise Four'));
 
 // Set up app so that it runs when this file is run
 app.listen(port, () => 
